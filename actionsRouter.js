276fd6c79031706194e6400c1bfac047d5d73f9c
Ryan Lee <therealsassyfatcat@outlook.com>
2020-09-04 11:13:42 -0500

    saving

--- actionsRouter.js
+++ actionsRouter.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const server = require('./server');
 const router = express.Router();
+const Actions = require('./data/helpers/actionModel.js');
 
-
+// GET ALL ACTIONS
+router.get('/', (req, res) => {
+Actions.get()
+    .then(result => res.status(200).json({ data: result }))
+    .catch(err => res.status(500).json({ error: "Server error" }))
+})
 
 module.exports = router;
