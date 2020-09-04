9673930e83491f00a854bf3389c546a7ee76a9b0
Ryan Lee <therealsassyfatcat@outlook.com>
2020-09-04 12:04:53 -0500

    saving

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+require('dotenv').config();
+
 const server = require('./server.js');
 
 const port = process.env.PORT;
-server.listen(port, () => {
+server.listen(port || 4000, () => {
     console.log('\n* Server Running on http://localhost:4000 *\n')
 })
