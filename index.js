a1e5a5cd69fde1e5311350099ba5c5485c4ed591
Ryan Lee <therealsassyfatcat@outlook.com>
2020-09-04 12:10:30 -0500

    saving

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ require('dotenv').config();
 const server = require('./server.js');
 
 const port = process.env.PORT;
-server.listen(port || 4000, () => {
+server.listen(port, () => {
     console.log('\n* Server Running on http://localhost:4000 *\n')
 })
