8a524bf2816420256389113fc42deda2be2a68fb
Ryan Lee <therealsassyfatcat@outlook.com>
2020-09-04 11:56:40 -0500

    saving

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ Go code!
 const server = require('./server.js');
 
 const port = process.env.PORT;
-server.listen(port | 4000, () => {
+server.listen(port, () => {
     console.log('\n* Server Running on http://localhost:4000 *\n')
 })
